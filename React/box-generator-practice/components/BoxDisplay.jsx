eb4bbb371c8b09e4d47c43fcfb3d90b163c1a2aa
Wyatt Ptak <107960001+Kino8724@users.noreply.github.com>
2024-03-10 13:28:09 -0700

    Updating to add delete button to box

--- React/box-generator-practice/components/BoxDisplay.jsx
+++ React/box-generator-practice/components/BoxDisplay.jsx
@@ -1,6 +1,12 @@
 const BoxDisplay = (props) => {
   const { boxes } = props
   console.log(boxes)
+
+  const deleteBox = (e) => {
+    console.log(e)
+    e.target.parentElement.remove()
+  }
+
   return (
     <>
       <h1>Boxes</h1>
@@ -9,7 +15,10 @@ const BoxDisplay = (props) => {
           boxes.map(box => {
             return (
               <>
-                <div style={{ backgroundColor: box.color, width: +box.size, height: +box.size }} />
+                <div style={{ backgroundColor: box.color, width: +box.size, height: +box.size }} >
+                  <button onClick={deleteBox} >X</button>
+
+                </div>
               </>
             )
           })
